f5b0b9cb37e4c5f7d5b3e91e91789c6e6e54a24f
gowthamr92 <gwthmr92@gmail.com>
2020-12-23 16:13:23 +0530

    Protractor Demo

---
+++ test/Test_Project_1.js
@@ -0,0 +1,34 @@
+describe('Demo project', function () {
+
+    it('Addition', function () {
+
+        browser.get('http://juliemr.github.io/protractor-demo/');
+
+        element(by.model('first')).sendKeys('5');
+        element(by.model('second')).sendKeys('51');
+        element(by.css('[ng-click="doAddition()"]')).click();
+
+        browser.sleep(3000);
+
+    });
+
+
+    it('subtraction', function () {
+
+        browser.get('http://juliemr.github.io/protractor-demo/');
+
+        element(by.model('first')).sendKeys('51');
+
+
+        var select = element(by.model('operator'));
+        select.$('[value="SUBTRACTION"]').click();
+        // element.all(by.options('SUBTRACTION')).click();
+
+
+        element(by.model('second')).sendKeys('5');
+        element(by.css('[ng-click="doAddition()"]')).click();
+
+        browser.sleep(3000);
+
+    });
+});
